1749574047eb78ad10c16b50f6e0196e2030d672
Maisa Maximo <maisa_maximo@outlook.com>
2019-10-24 16:07:50 -0300

    add footer and contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { HomeComponent } from './home/home.component';
+import { MissionComponent } from './mission/mission.component';
+import { ValueComponent } from './value/value.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HeroComponent,
+    HomeComponent,
+    MissionComponent,
+    ValueComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
